a6bbe2c9075424fa990665f01710ce7a1195dec2
wang18317838910 <qiaqnfeng@congcong.local>
2016-01-04 19:36:10 +0800

    Initial Commit

---
+++ 不定高cell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  不定高cell
+//
+//  Created by qiaqnfeng on 16/1/4.
+//  Copyright © 2016年 CCW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 不定高cell/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  不定高cell
+//
+//  Created by qiaqnfeng on 16/1/4.
+//  Copyright © 2016年 CCW. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
